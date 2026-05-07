24f0ed36f288aab2b8611f5cfa3d51da5ab4c616
Ray <shreyanair2104@gmail.com>
2026-05-07 21:16:20 +0530

    axios file to make requests to backend

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+//this is a axios file  to make requests to the backend 
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api', // Point to our Express backend
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Automatically add token to requests if it exists
+api.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.token) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
