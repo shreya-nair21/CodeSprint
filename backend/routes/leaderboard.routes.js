5f8ec4bef12d4bb3fa472a61c83e671de029369f
Ray <shreyanair2104@gmail.com>
2026-05-12 22:34:47 +0530

    add protectRoute

--- backend/routes/leaderboard.routes.js
+++ backend/routes/leaderboard.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { getLeaderboard } from '../controllers/leaderboard.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/', getLeaderboard);
+router.get('/', protectRoute, getLeaderboard);
 
 export default router;
