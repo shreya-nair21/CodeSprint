8ea34873568f6990a1fd33dbf5473f6c3a0d963f
Ray <shreyanair2104@gmail.com>
2026-05-13 23:26:47 +0530

    add optionalProtect route

--- backend/routes/problems.routes.js
+++ backend/routes/problems.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { getAllProblems, getProblemById, createProblem } from '../controllers/problems.controller.js';
-import { protectRoute, adminRoute } from '../middleware/auth.middleware.js';
+import { protectRoute, adminRoute, optionalProtect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/', getAllProblems);
+router.get('/', optionalProtect, getAllProblems);
 router.get('/:id', getProblemById);
 router.post('/', protectRoute, adminRoute, createProblem); // restricted to admin
 
