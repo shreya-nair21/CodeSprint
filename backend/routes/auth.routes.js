0730183e9fa0f8f5cb0e5127525415e6e6c71659
Ray <shreyanair2104@gmail.com>
2026-05-13 23:26:07 +0530

    add route for user stats

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getMe } from '../controllers/auth.controller.js';
+import { registerUser, loginUser, getMe, getUserStats } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protectRoute, getMe);
+router.get('/stats', protectRoute, getUserStats);
 
 export default router;
