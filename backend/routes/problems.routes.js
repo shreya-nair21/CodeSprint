69839a4103f5b74e02d0d84c6124cc52d3bdc52b
Ray <shreyanair2104@gmail.com>
2026-05-17 00:52:59 +0530

    edit

--- backend/routes/problems.routes.js
+++ backend/routes/problems.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllProblems, getProblemById, createProblem } from '../controllers/problems.controller.js';
+import { getAllProblems, getProblemById, createProblem, updateProblem, deleteProblem } from '../controllers/problems.controller.js';
 import { protectRoute, adminRoute, optionalProtect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get('/', optionalProtect, getAllProblems);
 router.get('/:id', getProblemById);
 router.post('/', protectRoute, adminRoute, createProblem); // restricted to admin
+router.put('/:id', protectRoute, adminRoute, updateProblem); // restricted to admin
+router.delete('/:id', protectRoute, adminRoute, deleteProblem); // restricted to admin
 
 export default router;
