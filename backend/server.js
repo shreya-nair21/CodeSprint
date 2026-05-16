337ceedfb93a7e513e81a5a893c77ea257a7dd8e
Ray <shreyanair2104@gmail.com>
2026-05-16 15:36:26 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ import commentRoutes from './routes/comments.routes.js';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 
 // Connect to MongoDB
