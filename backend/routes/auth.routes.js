00c146233240a0e0b9a7fc0ad0f27a98e88b308b
Ray <shreyanair2104@gmail.com>
2026-05-16 15:37:15 +0530

    add profile route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, getMe, getUserStats } from '../controllers/auth.controller.js';
+import { registerUser, loginUser, getMe, getUserStats, updateProfile } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/me', protectRoute, getMe);
 router.get('/stats', protectRoute, getUserStats);
+router.put('/profile', protectRoute, updateProfile);
 
 export default router;
