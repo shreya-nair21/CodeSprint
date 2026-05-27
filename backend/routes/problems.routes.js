422842adc24c49407321a176eb1bc6588f3c2b4f
Ray <shreyanair2104@gmail.com>
2026-05-27 23:47:43 +0530

    POST method for bookmark

--- backend/routes/problems.routes.js
+++ backend/routes/problems.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getAllProblems, getProblemById, createProblem, updateProblem, deleteProblem } from '../controllers/problems.controller.js';
+import { getAllProblems, getProblemById, createProblem, updateProblem, deleteProblem, toggleBookmark } from '../controllers/problems.controller.js';
 import { protectRoute, adminRoute, optionalProtect } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/', optionalProtect, getAllProblems);
-router.get('/:id', getProblemById);
+router.get('/:id', optionalProtect, getProblemById);
+router.post('/:id/bookmark', protectRoute, toggleBookmark);
 router.post('/', protectRoute, adminRoute, createProblem); // restricted to admin
 router.put('/:id', protectRoute, adminRoute, updateProblem); // restricted to admin
 router.delete('/:id', protectRoute, adminRoute, deleteProblem); // restricted to admin
